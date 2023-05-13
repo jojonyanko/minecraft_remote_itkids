3be7b679e7a8153caa73e86680bd1d52b1626ecf
jojonyanko <majoanma8@gmail.com>
2023-05-13 16:17:24 +0900

    1.12.2

--- itkids_m5/api_01_JA_jojonyanko/MY_API.py
+++ itkids_m5/api_01_JA_jojonyanko/MY_API.py
@@ -1,4 +1,21 @@
 from mcpi.minecraft import Minecraft
 import param_MCJE1122 as param
+import make_story
+import keyboard
+import make_all
 
 mc = Minecraft.create(port=param.PORT_MC)
+
+mc.postToChat("Please push Z")
+# make_all.make_ALL(mc,0,0,0,True,False,True,False,False,False,False)
+language = 0
+
+make_story.Story(mc,"Hello")
+make_story.Story(mc,"Which is your language Japanese or English? If it is Japanese, please push J. If it is English, please push E")
+while keyboard.is_pressed:
+    if keyboard.is_pressed("j"):
+        language = 1
+        mc.postToChat("へぇ、日本人だったのか")
+    if keyboard.is_pressed("e"):
+        language = 0
+        mc.postToChat("Oh, you use at English right? I see.")
---
+++ itkids_m5/api_01_JA_jojonyanko/make_story.py
@@ -0,0 +1,11 @@
+from mcpi.minecraft import Minecraft
+import param_MCJE1122 as param
+import keyboard
+
+mc = Minecraft.create(port=param.PORT_MC)
+
+def Story(mc,story):
+    while keyboard.is_pressed("z") == False:
+        while keyboard.is_pressed("z"):
+            mc.postToChat(story)
+            break
