ff27c1cda2ada3bcf15ada56ab2b27f926fdde95
jojonyanko <majoanma8@gmail.com>
2023-06-18 22:07:54 +0900

    MobをSummonできないコードの例

--- itkids_m5/api_01_JA_jojonyanko/hello_MCJE1122.py
+++ itkids_m5/api_01_JA_jojonyanko/hello_MCJE1122.py
@@ -1,7 +1,9 @@
 # Hello World for Minecraft Java Edition 1.12.2 or earlier
-from mcpi.minecraft import Minecraft
+from mcje.minecraft import Minecraft
 import param_MCJE1122 as param
+import mcpi.entity as entity
 
 mc = Minecraft.create(port=param.PORT_MC)
 mc.postToChat('Hello Minecraft Java Edition 1.12.2')
 mc.setBlocks(-5, 63, 5,  -5, 127, 5,  param.GOLD_BLOCK)
+mc.spawnEntity(0,0,0,entity.WITHER)
